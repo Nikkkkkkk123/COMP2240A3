43a2c77847e841bfe53cf328a1297e122c29145d
Nikk <c3362623@uon.edu.au>
2024-10-02 09:01:08 +1000

    Adjusted to work with VariableLTR
    added a default constructor and more methods to allow use to apply for VariableLTR

--- Frame.java
+++ Frame.java
@@ -2,6 +2,21 @@ class Frame {
     private int ownerProcess;
     private int pageNumber;
 
+    /*
+     * Description: Default constructor for the Frame class
+     * Parameters: None
+     * Returns: none
+     */
+    public Frame () {
+        ownerProcess = -1;
+        pageNumber = -1;
+    }
+
+    /*
+     * Description: Constructor for the Frame class
+     * Parameters: ownerProcess
+     * Returns: none
+     */
     public Frame(int ownerProcess) {
         this.ownerProcess = ownerProcess;
         pageNumber = -1;
@@ -9,6 +24,14 @@ class Frame {
 
     public void setPageNumber(int pageNumber) {
         this.pageNumber = pageNumber;
+    }
+
+    public void setOwnerProcess(int ownerProcess) {
+        this.ownerProcess = ownerProcess;
+    }
+
+    public int getOwnerProcess() {
+        return ownerProcess;
     }
 
     public int getPageNumber() {
